7bc8c78a6fbe9a388dc4ffcdc13d53e72c5684b3
Akhilvarma12 <akhilvarma2004@gmail.com>
2025-10-14 22:23:09 +0530

    added next auth logic and also defined custom modules in next-auth

--- src/helpers/sendVerificationEmail.ts
+++ src/helpers/sendVerificationEmail.ts
@@ -1,6 +1,6 @@
 import {resend} from '@/lib/resend';
 // import {VerificationEmail} from '@/emails/VerificationEmail';
-import { ApiResponse } from '../../types/ApiResponse';
+import { ApiResponse } from '../types/ApiResponse';
 import VerificationEmail from '../../emails/VerificationEmail';
 
 export const sendVerificationEmail = async (email: string,
