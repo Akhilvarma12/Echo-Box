b6e3c3b6b1eda5a0058bfd02ff3f9f688d7e8d03
Akhilvarma12 <akhilvarma2004@gmail.com>
2025-10-28 19:49:11 +0530

    added sign-up form using react-hook-form

---
+++ src/app/(auth)/sign-up/page.tsx
@@ -0,0 +1,166 @@
+'use client'
+import { useForm } from 'react-hook-form'
+import * as z from 'zod'
+import Link from 'next/link'
+import { useEffect, useState } from 'react';
+import { useDebounceCallback } from 'usehooks-ts'
+import { toast } from "sonner"
+import { useRouter } from 'next/navigation';
+import { signUpSchema } from '@/schemas/signUpSchema';
+import { zodResolver } from '@hookform/resolvers/zod';
+import axios, { AxiosError } from 'axios';
+import { ApiResponse } from '@/types/ApiResponse';
+import { Form, FormField, FormItem, FormLabel, FormControl, FormMessage } from '@/components/ui/form'
+import { Input } from '@/components/ui/input'
+import { Button } from '@/components/ui/button'
+import { Loader2 } from 'lucide-react';
+
+function page() {
+  const [username, setUsername] = useState('');
+  const [usernameMessage, setUsernameMessage] = useState('');
+  const [isCheckingUsername, setIsCheckingUsername] = useState(false);
+  const [isSubmitting, setIsSubmitting] = useState(false);
+
+  const debounced = useDebounceCallback(setUsername, 500);
+  const router = useRouter();
+
+  const form = useForm<z.infer<typeof signUpSchema>>({
+    resolver: zodResolver(signUpSchema),
+    defaultValues: {
+      username: '',
+      email: '',
+      password: ''
+    }
+  })
+
+  useEffect(() => {
+    const checkUsernameUnique = async () => {
+      if (username) {
+        setIsCheckingUsername(true);
+        setUsernameMessage('');
+        try {
+          const response = await axios.get(`/api/check-username?username=${username}`);
+          console.log(response.data.message)
+          setUsernameMessage(response.data.message);
+        } catch (error) {
+          const AxiosError = error as AxiosError<ApiResponse>;
+          setUsernameMessage(AxiosError.response?.data.message ?? 'Error checking username');
+        } finally {
+          setIsCheckingUsername(false);
+        }
+      }
+    }
+    checkUsernameUnique();
+  }, [username])
+
+  const onSubmit = async (data: z.infer<typeof signUpSchema>) => {
+    setIsSubmitting(true);
+    try {
+      const response = await axios.post<ApiResponse>('/api/sign-up', data);
+      toast.success(response.data.message);
+      router.replace(`/verify/${data.username}`);
+      setIsSubmitting(false);
+    } catch (error) {
+      console.error(error);
+      const AxiosError = error as AxiosError<ApiResponse>;
+      toast.error(AxiosError.response?.data.message || 'Error signing up');
+      setIsSubmitting(false);
+    }
+  }
+
+  return (
+    <div className="flex justify-center items-center min-h-screen bg-gray-800">
+      <div className="w-full max-w-md p-8 space-y-8 bg-white rounded-lg shadow-md">
+        <div className="text-center">
+          <h1 className="text-4xl font-extrabold tracking-tight lg:text-5xl mb-6">
+            Welcome Back to True Feedback
+          </h1>
+          <p className="mb-4">Sign in to continue your secret conversations</p>
+        </div>
+
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className='space-y-6'>
+            {/* Username */}
+            <FormField
+              control={form.control}
+              name="username"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Username</FormLabel>
+                  <FormControl>
+                    <Input
+                      placeholder="Enter your username"
+                      {...field}
+                      onChange={(e) => {
+                        field.onChange(e);
+                        debounced(e.target.value);
+                      }}
+                    />
+                  </FormControl>
+                    {isCheckingUsername && <Loader2 className="animate-spin" />}
+                  {!isCheckingUsername && usernameMessage && (
+                    <p
+                      className={`text-sm ${
+                        usernameMessage === 'Username is available'
+                          ? 'text-green-500'
+                          : 'text-red-500'
+                      }`}
+                    >
+                      {usernameMessage}
+                    </p>
+                  )}
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            {/* Email */}
+            <FormField
+              control={form.control}
+              name="email"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Email</FormLabel>
+                  <FormControl>
+                    <Input placeholder="Enter your email" type="email" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            {/* Password */}
+            <FormField
+              control={form.control}
+              name="password"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Password</FormLabel>
+                  <FormControl>
+                    <Input placeholder="Enter your password" type="password" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <Button type="submit" className="w-full" disabled={isSubmitting}>
+              {isSubmitting ? (<>
+              <Loader2 className='mr-2 h-4 w-4 animate-spin'/>Please Wait
+              </>):("Sign Up")}
+            </Button>
+          </form>
+        </Form>
+
+        <p className="text-sm text-center mt-4">
+          Already have an account?{' '}
+          <Link href="/sign-in" className="text-blue-600 hover:underline">
+            Sign in
+          </Link>
+        </p>
+      </div>
+    </div>
+  )
+}
+
+export default page
--- src/schemas/signUpSchema.ts
+++ src/schemas/signUpSchema.ts
@@ -1,15 +1,13 @@
 import { z } from "zod";
 
-export const userNameValidation =z
-    .string()
-    .min(3, "Username must be at least 3 characters long" )
-    .max(20, "Username must be at most 20 characters long")
-    .regex(/^[a-zA-Z0-9_]+$/, "Username can only contain letters, numbers, and underscores");
+export const userNameValidation = z
+  .string()
+  .min(3, "Username must be at least 3 characters long")
+  .max(20, "Username must be at most 20 characters long")
+  .regex(/^[a-zA-Z0-9_]+$/, "Username can only contain letters, numbers, and underscores");
 
 export const signUpSchema = z.object({
-    username: userNameValidation,
-    email: z.email()
-    .refine(() => false, { message: "Invalid email" }),
-    password: z.string().min(6, "Password must be at least 6 characters long"),
-
-})
+  username: userNameValidation,
+  email: z.string().email("Invalid email"),
+  password: z.string().min(6, "Password must be at least 6 characters long"),
+});
